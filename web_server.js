6b205085b3a92eeed01813b221c233816c623942
Tamara Bartlett <tbartlet@thoughtworks.com>
2016-10-24 20:12:29 -0500

    Writes super simple node server to host index.html

---
+++ web_server.js
@@ -0,0 +1,15 @@
+var http = require("http"),
+	fs = require("fs")
+
+var server = http.createServer(function(request, response) {
+	var filename = "index.html"
+
+	fs.readFile(filename, "binary", function(err, file) {
+      response.writeHead(200);
+      response.write(file, "binary");
+      response.end();
+    });
+});
+
+server.listen(8080);
+console.log("Server is listening");
